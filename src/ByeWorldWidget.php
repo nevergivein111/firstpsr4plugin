cc6fa6caab4216d0bd9ed37d645a214a50b050ae
Mohsin <nevergivein111@gmail.com>
2015-06-03 16:45:19 +0500

    first commit

---
+++ src/ByeWorldWidget.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace nexus;
+
+//use yii\base\Widget;
+//use yii\helpers\Html;
+
+class ByeWorldWidget //extends Widget
+{
+public $message;
+
+public function __construct()
+{
+//parent::init();
+if ($this->message === null) {
+$this->message = 'Bye World';
+}
+}
+
+public function run()
+{
+return '<strong>'.$this->message.'</strong>';
+}
+}
